261758a6fceddb6a17e5663b6c398370395ad77e
Fadime35 <fadimekoc2111@gmail.com>
2024-08-15 10:04:50 +0300

    water input

---
+++ KaloriWebApplication/Controllers/WaterController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using KaloriWebApplication.Models;
+using KaloriWebApplication.Models.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System;
+using System.Security.Claims;
+
+namespace KaloriWebApplication.Controllers
+{
+    public class WaterController: Controller
+    {
+        private readonly Context _context;
+
+        public WaterController(Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
